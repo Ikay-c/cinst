c7195e72125d3e10108aeed5f21c30bbccadf8e3
cyril <iducyril@gmail.com>
2020-09-25 21:22:59 +0100

    initial

--- scripts/index.js
+++ scripts/index.js
@@ -27,11 +27,11 @@ function Chatbot(){
   
   this.messages = "";
   this.start = function(){
-  
+  };
   // here's a constructor function that serves like a house to the message body.
-  function createMessage(input, messageBody, type = 'output'){
+  function createMessage(messages, messageBody, type = 'output'){
     var box = document.createElement('div');
-    box.setAttribute('class', 'chat-message');
+    box.setAttribute('class', 'chat-box')
     var text = document.createElement('span');
     if (type == 'output'){
       text.style.color = 'violet';
@@ -40,34 +40,35 @@ function Chatbot(){
       text.style.color = 'pink';
       text.setAttribute('class', 'text-receive');
     }
-    messageBody.appendChild(box);
-    text.innerHTML = message;
+    box.appendChild(text);
+    self.messageBody.appendChild(box);
+    text.innerHTML = self.messages;
   }
-  //here the method receives the message from the user and send to the message body.
-  var receive = function(input){
-    this.messages = input;
-    createMessage(input, this.messageBody, 'receive');
+  //here the method receives the message from the user and send to the message bdy.
+  this.receive = function(messages){
+    this.messages = messages;
+    createMessage(messages, this.messageBody, 'receive');
   };
   var self = this;
   function chat(){// here is a function that tries to loop through the items in the array.
-    var inputMessage = self.input.value;
+    var inputMessage = self.inputBox.value;
     if(self.messages != inputMessage){//here we try to compare the user's input to the 
-      receive(inputMessage);
+      self.receive(inputMessage);//passing the input message through the receive method.
       /* i here is for the arrays inside the outermost array with length of 3*/
       var i = 0;
-      while(i < self.innerMessages){
+      while(i < self.message.length){
         self.innerMessages = self.message[i];
         /* k is for the elements inside the inner arrays*/
         k = 0;
         while (k < self.innerMessages.length){
          self.userInput = self.innerMessages[k];
-         var confirm = self.userInput.includes(self.messages);
+         var confirm = self.messages.includes(self.userInput);
          if (confirm == true){
           self.innerReplies = self.replies[i];
-          self.send(self.innerReplies[k]);
+          self.output(self.innerReplies[k]);//passing the response to thr user
+          break;
          }else{
           k++;
-          break;
          }
         }
       i++;
@@ -75,15 +76,14 @@ function Chatbot(){
    }
   }
   
-  this.output= function(input){//here is a method that replies a user with a marches response through the user interface.
-    createMessage(message, this.messageBody, 'output');
+  this.output= function(messages){//here is a method that replies a user with a matched response through the user interface.
+    createMessage(messages, this.messageBody, 'output');
   };
   this.sendButton.onclick = function(events){//here is an event that sends the user's message once clicked.
     events.preventDefault();
     chat();
   };
- };
-}
-var chat = new Chatbot();//creating an object
-chat.start();//starting the new object function.
+ }
+var chater = new Chatbot();//creating an object
+chater.start();//starting the new object function.
 // the whole code works like this: a user interface where the user interacts with the bot is created. it has a box that houses every other thing and that box is the chat body. inside the chat body, there's another box thaf contains the messages from the user and the bot and that box is called the message body. inside thag chat body, there's another box that allows a user to type his or her message and that box is called the input box. beside the input box is another box that sends the input message and that box is called the send button. When the user types, it compares the input to the array, sends it to the bot through the receive method form the create message function.
